67676efe448ad9d05696c03e0f614a944d1b80bb
Julien Brunel <julien.brunel@onera.fr>
2020-10-16 11:55:18 +0200

    v1 of TTC

--- src/transfo_TTC.ml
+++ src/transfo_TTC.ml
@@ -5,7 +5,25 @@ let find_transitive_closure m rel =
     | None -> None
     | Some p -> Some p.tc
 
- let closure_axiom rel var vars fml =
-   (* let propagate rel fml_from_x = (true) in *)
-    (rel, var, vars, fml)
+ let closure_axiom m rel x vars fml =
+    let s = x.var_sort in 
+    let x_propag = make_variable ~var_name:(Name.make_unloc "_ttc_propagate_x") ~var_sort:s in
+    let y_propag = make_variable ~var_name:(Name.make_unloc "_ttc_propagate_y") ~var_sort:s in
+    
+    let propagate = all x_propag (all y_propag @@ always (implies (and_  (substitute x x_propag fml) (lit @@ pos_app 0 rel [var x_propag; var y_propag])) 
+                                      (eventually @@ substitute x y_propag fml)))
+    in
+    let fresh_x = make_variable ~var_name:(Name.make_unloc "_ttc_x") ~var_sort:s in
+    let fresh_y = make_variable ~var_name:(Name.make_unloc "_ttc_y") ~var_sort:s in
+    match (find_transitive_closure m rel) with
+    | None -> true_
+    | Some (tc_rel) ->
+      all fresh_x (all fresh_y @@ 
+        List.fold_right (fun cur_var cur_fml -> all cur_var cur_fml) vars 
+        (implies
+          propagate
+          (always @@ implies (and_  (substitute x fresh_x fml) (lit @@ pos_app 0 tc_rel [var fresh_x; var fresh_y])) (eventually @@ substitute x fresh_y fml)
+          )
+        )
+      )
 
