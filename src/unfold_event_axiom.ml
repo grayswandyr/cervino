cd58e9efade3c454a5934976ea1674ed6464b1c4
David Chemouil <david.chemouil@onera.fr>
2021-01-15 16:41:30 +0100

    refined event axiom unfolding (only one constant if not 'temporal')

--- src/unfold_event_axiom.ml
+++ src/unfold_event_axiom.ml
@@ -16,29 +16,29 @@ let substitute (subst : (variable * term) list) (t : term) : term =
 (* unfolds the (G exists ...) event axioms by intantiating every existential quantifier with 2 fresh constants (that must not be considered by instantiation passes). *)
 let unfold_event_formula axioms =
   (* keep already created fresh constants (as we will meet the same variable several times, we don't want to create new fresh constants for them) *)
-  let constants : (variable * (constant * constant)) list ref = ref [] in
-  (* retrieves (if they exist) or creates 2 fresh constants for a given bound variable *)
-  let constants_for_bound_var ({ var_name; var_sort } as var) =
+  let constants : (variable * constant list) list ref = ref [] in
+  (* retrieves (if they exist) or creates 1 or 2 (depends on formula temporality) fresh constants for a given bound variable *)
+  let constants_for_bound_var ({ var_name; var_sort } as var) fml =
     match List.assoc_opt ~eq:equal_variable var !constants with
     | None ->
+        let nb = if fst (nb_next fml) then 2 else 1 in
         let base_name = Name.content var_name in
-        let c1 =
-          make_constant ~cst_name:(Name.fresh base_name) ~cst_sort:var_sort
+        let cs =
+          List.init nb (fun _ ->
+              make_constant ~cst_name:(Name.fresh base_name) ~cst_sort:var_sort)
         in
-        let c2 =
-          make_constant ~cst_name:(Name.fresh base_name) ~cst_sort:var_sort
-        in
-        constants := (var, (c2, c1)) :: !constants;
-        (c1, c2)
-    | Some c1c2 ->
-        c1c2
+        constants := (var, cs) :: !constants;
+        cs
+    | Some cs ->
+        cs
   in
   (* walk through a formula that began with G immediately followed by exists and unfold existential quantifiers on ad-hoc (fresh) constants. A substitution keeps track of each constant associated to a bound variabe. *)
   let rec unfold (subst : (variable * term) list) fml =
     match fml with
     | Exists (_, v, f) ->
-        let c1, c2 = constants_for_bound_var v in
-        or_ (unfold ((v, cst c1) :: subst) f) (unfold ((v, cst c2) :: subst) f)
+        let cs = constants_for_bound_var v f in
+        let fmls = List.map (fun c -> unfold ((v, cst c) :: subst) f) cs in
+        disj fmls
     | All (folding_csts, v, f) ->
         (* univ. quantifiers may hide a previous existential binding, so we remove any corresponding association from the substitution before walking down *)
         let subst' =
@@ -74,9 +74,7 @@ let unfold_event_formula axioms =
   in
   let updated_axioms = List.map change_event_axiom axioms in
   (* retrieves the newly created constants *)
-  let fresh_constants =
-    List.flat_map (fun (_, (c1, c2)) -> [ c1; c2 ]) !constants
-  in
+  let fresh_constants = List.flat_map snd !constants in
   (fresh_constants, updated_axioms)
 
 
