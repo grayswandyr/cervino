928a3dfa61af2e1ddad40ad1ccdce745a55edd9a
bodeveix <bodeveix@irit.fr>
2021-04-13 13:54:19 +0200

    skolem

--- coq/skolem.v
+++ coq/skolem.v
@@ -25,143 +25,90 @@ Section Skolem.
   Context {Ts: Type} {Tv: Ts->Type} {Tc: Ts->Type} {Tp: Type}.
   Variable Sg: @Sig Ts Tv Tc Tp.
   
-  Lemma tfr_spec: forall f,
-      { xT : Sort (Sig:=Sg) -> Type & { xF : forall (s: Sort (Sig:=Sg)), @Finite (xT s) & {f' : formula (cextSig Sg xF) | 
-        forall (D: Dom Sg) (itp: Interp D) (env: Env Sg D) t,
+  Fixpoint newVarType f : Sort -> Type := 
+  match f with
+    And _ f1 f2 => fun s => (newVarType f1 s + newVarType f2 s)%type
+  | Or _ f1 f2 => fun s => (newVarType f1 s + newVarType f2 s)%type
+  | Ex _ s v f' => fun s' => ((if eq_dec s s' then One else Empty) + newVarType  f' s')%type
+  | F _ f' => newVarType f'
+  | _ => fun s => Empty
+  end.
+
+  Fixpoint newVar f : forall s, @Finite (newVarType f s) := 
+  match f return forall s, @Finite (newVarType f s) with
+    And _ f1 f2 => fun s => SumFin (newVar f1 s) (newVar f2 s)
+  | Or _ f1 f2 => fun s => SumFin (newVar f1 s) (newVar f2 s)
+  | Ex _ s v f' => fun s' => SumFin (if eq_dec s s' as e return @Finite (if e then One else Empty) then OneFin else EmptyFin) (newVar  f' s')
+  | F _ f' => newVar f'
+  | _ => fun s => EmptyFin
+  end.
+  
+  Definition sk_sg f := cextSig Sg (newVar f).
+  
+  Fixpoint sk_fm f : formula (sk_sg f) :=
+  match f return formula (sk_sg f) with
+    And _ f1 f2 => And (cext2Sig Sg (newVar f1) (newVar f2)) 
+      (cinj1_fm Sg (newVar f1) (newVar f2) (sk_fm f1)) 
+      (cinj2_fm Sg (newVar f1) (newVar f2) (sk_fm f2)) 
+  | Or _ f1 f2 => Or (cext2Sig Sg (newVar f1) (newVar f2)) 
+      (cinj1_fm Sg (newVar f1) (newVar f2) (sk_fm f1)) 
+      (cinj2_fm Sg (newVar f1) (newVar f2) (sk_fm f2))
+  | Ex _ s v f' =>
+    let xF' s' := if eq_dec s s' as e return @Finite (if e then One else Empty) then OneFin else EmptyFin in
+    let f'' := cinj2_fm Sg xF' (newVar f') (sk_fm f') in 
+    let c := match eq_dec s s as e return if e then OneFin else EmptyFin with
+        left e => one
+      | right n => match n eq_refl with end
+      end in
+    csubst (cextSig Sg (fun s' => SumFin (xF' s') (newVar f' s'))) v (inr (inl c))  f''
+  | F _ f' => F _ (sk_fm f')
+  | _ => cext_fm _ f
+  end.
+
+  Lemma sk_fm_sem: forall f (D: Dom Sg) (itp: Interp D) (env: Env Sg D) t,
           fm_sem Sg (Itp:=itp) env f t ->
-            exists (xD: forall s, xT s -> ssem s), fm_sem (cextSig Sg xF) (Itp:=cext_itp Sg itp xF xD) (cext_env Sg xF env) f' t
-       }}}.
+            exists (xD: forall s, newVar f s -> ssem s), fm_sem (cextSig Sg (newVar f)) (Itp:=cext_itp Sg itp (newVar f) xD) (cext_env Sg (newVar f) env) (sk_fm f) t.
   Proof.
-    induction f; intros; auto.
-    - exists (fun s => Empty); exists (fun s => EmptyFin).
-      exists (cext_fm (fun s => EmptyFin) (FTrue Sg)).
-      intros.
-      exists (fun s x => match x in Empty with end).
-      revert H; apply cext_sem.
-    - exists (fun s => Empty); exists (fun s => EmptyFin).
-      exists (cext_fm (fun s => EmptyFin) (FFalse Sg)).
-      intros.
-      inversion H.
-    - exists (fun s => Empty); exists (fun s => EmptyFin).
-      exists (cext_fm (fun s => EmptyFin) (Atom Sg a)).
-      intros.
-      exists (fun s x => match x in Empty with end).
-      revert H; apply cext_sem.
-    - destruct IHf1 as [xT1 [xF1 [f'1 h1]]].
-      destruct IHf2 as [xT2 [xF2 [f'2 h2]]].
-      exists (fun s => (xT1 s + xT2 s)%type).
-      exists (fun s => SumFin (xF1 s) (xF2 s)).
-      exists (And (cext2Sig Sg xF1 xF2) (cinj1_fm Sg xF1 xF2 f'1) (cinj2_fm Sg xF1 xF2 f'2)).
-      intros.
-      destruct H as [H1 H2].
-      apply h1 in H1; clear h1.
-      apply h2 in H2; clear h2.
-      destruct H1 as [xD1 H1].
-      destruct H2 as [xD2 H2].
+    induction f; intros; try (exists (fun s x => match x in Empty with end); auto; revert H; apply cext_sem).
+    - destruct H as [H1 H2].
+      apply IHf1 in H1; clear IHf1; apply IHf2 in H2; clear IHf2.
+      destruct H1 as [xD1 H1]; destruct H2 as [xD2 H2].
       exists (fun s c => match c with inl c1 => xD1 s c1 | inr c2 => xD2 s c2 end).
       apply And_sem; split.
       now apply cinj1_sem.
       now apply cinj2_sem.
-    - destruct IHf1 as [xT1 [xF1 [f'1 h1]]].
-      destruct IHf2 as [xT2 [xF2 [f'2 h2]]].
-      exists (fun s => (xT1 s + xT2 s)%type).
-      exists (fun s => SumFin (xF1 s) (xF2 s)).
-      exists (Or (cext2Sig Sg xF1 xF2) (cinj1_fm Sg xF1 xF2 f'1) (cinj2_fm Sg xF1 xF2 f'2)).
-      intros.
-      destruct H as [H|H].
-      apply h1 in H; destruct H as [xD1 H].
-      exists (fun s c => match c with inl c1 => xD1 s c1 | inr c2 => neDom s end).      
-      apply Or_sem; left.
-      apply cinj1_sem; now apply H.
-      apply h2 in H; destruct H as [xD2 H].
-      exists (fun s c => match c with inr c2 => xD2 s c2 | inl c1 => neDom s end).
-      apply Or_sem; right.
-      apply cinj2_sem; now apply H.
-    - destruct IHf as [xT [xF [f' H]]].
-      set (xT' s' := if eq_dec s s' return Type then One else Empty).
-      exists (fun s => (xT' s + xT s)%type).
-      set (xF' s' := if eq_dec s s' as e return @Finite (if e then One else Empty) then OneFin else EmptyFin).
-      exists (fun s => SumFin (xF' s) (xF s)).
-      set (f'' := cinj2_fm Sg xF' xF f').
-      set (c := match eq_dec s s as e return if e then OneFin else EmptyFin with
-        left e => one
-      | right n => match n eq_refl with end
-      end).
-      exists (csubst (cextSig Sg (fun s => SumFin (xF' s) (xF s))) e (inr (inl c)) f'').
-      intros.
-      generalize (proj1 (Ex_sem _ _ _ _ s e f t) H0); clear H0; intro.
-      destruct H0 as [d H0].
-      apply H in H0; clear H.
-      destruct H0 as [xD H0].
-      set (xD' := fun s' => match eq_dec s' s return xT' s' -> ssem s' with 
+    - destruct H as [H1|H2].
+      apply IHf1 in H1; clear IHf1.
+      destruct H1 as [xD1 H1].
+      exists (fun s c => match c with inl c1 => xD1 s c1 | inr c2 => neDom s end).
+      left; revert H1; now apply cinj1_sem.
+      apply IHf2 in H2; clear IHf2.
+      destruct H2 as [xD2 H2].
+      exists (fun s c => match c with inr c1 => xD2 s c1 | inl c2 => neDom s end).
+      right; revert H2; now apply cinj2_sem.
+    - destruct H as [d H].
+      apply IHf in H; clear IHf.
+      destruct H as [xD H].
+      set (xD' := fun s' => match eq_dec s' s return (if eq_dec s s' return Type then One else Empty) -> ssem s' with 
         left e => fun c' => rew <-[ssem] e in d 
       | right n => fun c' => neDom s'
       end).
-      exists (fun s c => match c with inl c1 => xD' s c1 | inr c2 => xD s c2 end).
+      exists (fun s' c => match c return ssem s' with inl c1 => xD' s' c1 | inr c2 => xD s' c2 end).
+      simpl.
       apply csubst_sem.
-      apply cinj2_sem with (xF1:=xF') (xD1:=xD') in H0.
-      simpl in H0|-*.
-      change (cext_env Sg (fun s0 : Sort => SumFin (xF' s0) (xF s0)) env) with env.
-      change (cext_env Sg xF (add Sg e d env)) with (add Sg e d env) in H0.
-      change (cextSig Sg (fun s0 : Sort => SumFin (xF' s0) (xF s0))) with (cext2Sig Sg xF' xF).
-      assert (xD' s c = d).
-      unfold xD'; clear H0 xD'.
-      unfold xT'.
-      destruct (eq_dec s s); try (exfalso; now auto).
-      rewrite (proof_irrelevance _ e0 eq_refl); simpl.
-      unfold eq_rect_r; reflexivity.
-      rewrite H.
-      unfold f''.
-      revert H0; apply fm_sem_equiv; intros.
+      apply cinj2_sem.
+      revert H; apply fm_sem_equiv; intros.
+      unfold xD'; simpl.
+      destruct (eq_dec s s); try tauto.
+      rewrite (proof_irrelevance _ e0 eq_refl).
       reflexivity.
-    - clear IHf.
-      exists (fun s => Empty); exists (fun s => EmptyFin).
-      exists (cext_fm (fun s => EmptyFin) (All Sg s e f)); intros.
-      exists (fun s c => match c in Empty with end).
-      revert H; apply cext_sem.
-    - destruct IHf as [xT [xF [f' H]]].
-      exists xT; exists xF; exists (F _ f'); intros.
-      destruct H0 as [t' [ht H0]].
-      apply H in H0; clear H.
-      destruct H0 as [xD H0].
+    - destruct H as [t' [ht H]].
+      apply IHf in H; clear IHf.
+      destruct H as [xD H].
       exists xD.
-      apply F_sem.
       exists t'; split; auto.
-    - clear IHf.
-      exists (fun s => Empty); exists (fun s => EmptyFin).
-      exists (cext_fm (fun s => EmptyFin) (G _ f)); intros.
-      exists (fun s c => match c in Empty with end).
-      revert H; apply cext_sem.
   Qed.
-
-  Definition sk_Tc (f: formula Sg) : Ts -> Type :=
-    fun s => match tfr_spec f with
-      existT _ xT _ => (constant s + xT s)%type
-    end.
-
-  Definition sk_Fc (f: formula Sg) : forall s, Finite (T:=sk_Tc f s).
-  Proof.
-    intros.
-    unfold sk_Tc.
-    destruct (tfr_spec f).
-    destruct s0 as [xF _].
-    exact (SumFin (constant s) (xF s)).
-  Defined.
   
-  Definition sk_sg (f: formula Sg): Sig (Ts := Ts) (Tv:=Tv) (Tc:=sk_Fc f) (Tp:=Tp).
-  Proof.
-    unfold sk_Fc, sk_Tc.
-    destruct (tfr_spec f).
-    destruct s as [xF _].
-    exact (cextSig Sg xF).
-  Defined.
-
-  Definition sk_fm (f:formula Sg): formula (sk_sg f).
-  Proof.
-    unfold sk_sg, sk_Fc, sk_Tc.
-    destruct (tfr_spec f) as [xT [xF [f' _]]].
-    exact f'.
-  Defined.
 End Skolem.
 
 (** PaperLemma 2 **)
@@ -169,13 +116,11 @@ Lemma skolem_sat: forall `(Sg: Sig) (f: formula Sg),
   isSat Sg f -> isSat (sk_sg Sg f) (sk_fm Sg f).
 Proof.
   intros.
-  destruct H as [D [itp [env [t h]]]].
-  unfold sk_fm, sk_sg, sk_Fc, sk_Tc.
-  destruct (tfr_spec Sg f) as [xT [xF [f' H]]].
-  specialize (H D itp env t h).
+  destruct H as [D [itp [env [t H]]]].
+  apply sk_fm_sem in H.
   destruct H as [xD H].
   exists (cext_dom _ _ D).
-  exists (cext_itp Sg itp xF xD).
+  exists (cext_itp Sg itp _ xD).
   exists env.
   exists t.
   apply H.
